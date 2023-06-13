c46c0f9288606aca03c19156e82878bcdb327521
Jayesh Popat <jpopat@juniper.net>
2023-06-13 11:14:03 +0530

    Code fix for adding the new repo link for tf-ansible-deployer and adding the missing roles for analytics

--- ansible/roles/contrail/filter_plugins/repo_filter.py
+++ ansible/roles/contrail/filter_plugins/repo_filter.py
@@ -20,11 +20,11 @@ class FilterModule(object):
         branches_info = tf_branches_info + contrail_branches_info
         repo = ''
         if branch_required == 'master':
-            return "https://github.com/tungstenfabric/tf-ansible-deployer.git"
+            return "https://ssd-git.juniper.net/tungstenfabric/tf-ansible-deployer.git"
         for branch in branches_info:
             if branch['name'] == branch_required:
                 repo = branch['commit']['url']
         if 'contrail-ansible-deployer' in repo:
             return "https://github.com/Juniper/contrail-ansible-deployer.git"
         else:
-            return "https://github.com/tungstenfabric/tf-ansible-deployer.git"
+            return "https://ssd-git.juniper.net/tungstenfabric/tf-ansible-deployer.git"
